d075ebc6b10e511134cca8871ee7817ccd041db6
TranViet2911 <107057085+TranViet2911@users.noreply.github.com>
2025-08-04 19:30:59 +0700

    Update ping.py

--- cogs/ping.py
+++ cogs/ping.py
@@ -1,14 +1,13 @@
-import discord
 from discord.ext import commands
 
-class ping(commands.Cog):
+class Ping(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
     async def ping(self, ctx):
-        latency = round(self.bot.latency * 1000)  # latency in ms
+        latency = round(self.bot.latency * 1000)
         await ctx.send(f'Pong! 🏓 {latency}ms')
 
-def setup(bot):
-    bot.add_cog(ping(bot))
+async def setup(bot):
+    await bot.add_cog(Ping(bot))
