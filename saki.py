12dd44d971e6c7a9eae1957533d59e9c35a47cb7
TranViet2911 <107057085+TranViet2911@users.noreply.github.com>
2025-08-04 19:04:40 +0700

    Add files via upload

--- saki.py
+++ saki.py
@@ -1,5 +1,7 @@
+# Import
 import discord
 from discord.ext import commands
+from discord import app_commands
 import logging
 from dotenv import load_dotenv
 import os
@@ -8,15 +10,34 @@ load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+logging.basicConfig(level=logging.DEBUG, handlers=[handler])
+
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+# Prefix Command
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
-	print(f"We are ready to go in, {bot.user.name}")
-	
+    print(f"{bot.user.name} đã được khởi động!")
+    
+# Load Cogs
 
-bot.run(token, log_handler=handler, log_level=logging.DEBUG)
+bot.load_extension('cogs.ping')
+
+# Rena gay :)
+@bot.event
+async def on_message(message):
+	if message.author == bot.user:
+		return
+		
+	if "rena" in message.content.lower():
+		await message.channel.send(f"gay")
+	
+	await bot.process_commands(message)
+bot.run(token)
