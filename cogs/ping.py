4b0fa77229c48f11d78f0a4f5628b6e183cd88ec
TranViet2911 <107057085+TranViet2911@users.noreply.github.com>
2025-08-04 20:47:47 +0700

    Update ping.py

--- cogs/ping.py
+++ cogs/ping.py
@@ -1,16 +1,17 @@
+import discord
 from discord.ext import commands
 
 class Ping(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @discord.app_commands.command(name = 'ping', description = 'Kiểm tra độ trễ của bot tới Discord')
+    @discord.app_commands.command(name='ping', description='Kiểm tra độ trễ của bot tới Discord')
     async def ping(self, interaction: discord.Interaction):
         latency = round(self.bot.latency * 1000)
         await interaction.response.send_message(f'Pong! 🏓 {latency}ms')
 
-		async def cog_load(self):
+    async def cog_load(self):
         await self.bot.tree.sync()
 
-		async def setup(bot):
-    		await bot.add_cog(Ping(bot))
+async def setup(bot):
+    await bot.add_cog(Ping(bot))
